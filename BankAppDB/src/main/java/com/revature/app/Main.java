d64c8617fcc98f5fceb384f3dcdb589833052991
Damian334 <dam.wadolowski@gmail.com>
2022-01-15 20:00:04 +0100

    BankAppDB with tests and database

--- BankAppDB/src/main/java/com/revature/app/Main.java
+++ BankAppDB/src/main/java/com/revature/app/Main.java
@@ -1,26 +1,9 @@
 package com.revature.app;
 
-import com.revature.entity.User;
-import com.revature.entity.dao.AccountDao;
-import com.revature.entity.dao.ConnectionManager;
-import com.revature.entity.dao.UserDao;
-import java.sql.Connection;
-
 public class Main {   
     
     public static void main(String[] args) {
         Bank b = new Bank();
-        b.run();
-        
-        //UserDao us = new UserDao();
-        //System.out.println("INSERT INTO users(name,password) VALUES (\'" + "any" + "\', \'" + "pass" + "\')");
-        //us.addUser("name", "pass");
-        //System.out.println(us.getUser("name"));
-        //User user = us.getUser("name");
-        
-        //System.out.println(user);
-        //AccountDao acc = new AccountDao();
-        //acc.addAccount("new acc", user);
-        
+        b.run();                
     }
 }
